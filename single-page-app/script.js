6448ab7ad1a28f189defd49b341bdc01d260a674
Rohit <mourya.rohit32@gmail.com>
2017-08-05 23:26:11 +0530

    Designed Single Page application using angular

---
+++ single-page-app/script.js
@@ -0,0 +1,34 @@
+// create module
+var mainApp=angular.module("mainApp",["ngRoute"]);
+
+mainApp.config(function($routeProvider){
+  $routeProvider
+  .when('/',{
+    templateUrl:'pages/home.html',
+    controller:"mainController"
+  })
+
+  .when("/about",{
+    templateUrl:"pages/about.html",
+    controller:"aboutController"
+  })
+
+  .when("/contact",{
+    templateUrl:"pages/contact.html",
+    controller:"contactController"
+  });
+});
+// create controller
+mainApp.controller("mainController",function($scope){
+  $scope.message="Welcome to home page";
+});
+
+// create controller
+mainApp.controller("aboutController",function($scope){
+  $scope.message="Welcome to about page";
+});
+
+// create controller
+mainApp.controller("contactController",function($scope){
+  $scope.message="Welcome to contact page";
+});
